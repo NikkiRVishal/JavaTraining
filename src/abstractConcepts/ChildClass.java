cb8664a675dbd27abc9bc8d89833b37fc56b137f
NikkiRVishal <114976315+NikkiRVishal@users.noreply.github.com>
2023-12-28 07:05:02 -0800

    Update ChildClass.java
    For testing purpose have done it

--- src/abstractConcepts/ChildClass.java
+++ src/abstractConcepts/ChildClass.java
@@ -4,6 +4,6 @@ public abstract class ChildClass extends ParentClass{
     // If parent class is having abstract method then in child class either we have to implement that method inside
     // child class or that child class we have to make as abstract class
   /*void test(){
-
+  //testing the commit from ui
     }*/
 }
